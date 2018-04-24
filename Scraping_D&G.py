fb68f778f45ea41b62463c52c40d26f4a890afd9
William <38208570+williamceli@users.noreply.github.com>
2018-04-24 13:51:03 +0100

    Update Scraping_D&G.py

--- Scraping_D&G.py
+++ Scraping_D&G.py
@@ -2,29 +2,32 @@
 """
 Created on Tue Apr 24 13:23:01 2018
 
-@author: WilliamCelli
+@author: WilliamCeli
 """
 
 import requests
-from   bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 
 base_url = "https://store.dolcegabbana.com/en/women/clothing/dresses/?page="
 prices = []
 
-for i in range(1,10):
+for i in range(1,100):
     url = base_url + str(i)
     with requests.session() as s:
         s.headers['user-agent'] = 'Mozilla/5.0'
-    try:     
-            r = s.get(url,timeout = 25)
-    except Exception as e:
-        print(e)
-    st = r.status_code
-    print('STATUS: ' + str(st))
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, "html.parser")
-        divs = soup.findAll('div', attrs = {"class":"b-product_price"})
-        for d in divs:
-            prices.append(d.text)
+        try:     
+                r = s.get(url,timeout = 25)
+        except Exception as e:
+            print(e)
+        st = r.status_code
+        print('STATUS: ' + str(st))
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, "html.parser")
+            divs = soup.findAll('div', attrs = {"class":"b-product_price"})
+            for d in divs:
+                prices.append(d.text)
+        elif r.status_code == 404:
+            print("Sorry, No more pages to show") 
+            break
                 
